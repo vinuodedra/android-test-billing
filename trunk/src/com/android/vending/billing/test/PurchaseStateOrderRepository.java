fa757190835159107ae30d36f95210ab2bf74803
daniel.lazarenko@gmail.com <daniel.lazarenko@gmail.com@fc87a38b-5754-c83f-0b7a-08c7104e22ce>
2011-09-16 22:43:52 +0000

    main tests implemented

---
+++ trunk/src/com/android/vending/billing/test/PurchaseStateOrderRepository.java
@@ -0,0 +1,31 @@
+package com.android.vending.billing.test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PurchaseStateOrderRepository {
+	private static PurchaseStateOrderRepository instance;
+	
+	public static PurchaseStateOrderRepository getInstance() {
+		if (instance == null)
+			instance = new PurchaseStateOrderRepository();
+		return instance;
+	}
+	
+	private Map<String, PurchaseStateOrder> orders = new HashMap<String, PurchaseStateOrder>();
+	
+	private PurchaseStateOrderRepository() {		
+	}
+	
+	public void add(PurchaseStateOrder order) {
+		if (order.getOrderId() == null) {
+			String newOrderId = (orders.size() + 1) + "";
+			order.setOrderId(newOrderId);
+		}
+		orders.put(order.getOrderId(), order);		
+	}
+	
+	public PurchaseStateOrder get(String orderId) {
+		return orders.get(orderId);
+	}
+}
